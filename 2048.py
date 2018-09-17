5ea3168ec69d5486098f11c08183cb2bb469f7e0
petoklung <32834322+petoklung@users.noreply.github.com>
2018-09-17 11:00:47 +0700

    Second commit via upload

--- 2048.py
+++ 2048.py
@@ -4,10 +4,10 @@ for i in range(4):
 direction = int(input("Insert your direction: "))
 print(data)
 
-# go left - 0 / go top - 1 / go right - 2 / go down - 3 
+# go left - 0 / go top - 1 / go right - 2 / go down - 3
 for x in range(len(data)):
     for y in range(len(data)-1):
-        print("X: ", x , ", Y: ", y, ", Data: ", data[x][y])
+        print("X: ", x, ", Y: ", y, ", Data: ", data[x][y])
         if(direction == 0):
             if(data[x][y+1] == data[x][y]):
                 data[x][y] = int(data[x][y]) + int(data[x][y+1])
@@ -16,9 +16,55 @@ for x in range(len(data)):
                 data[x][len(data)-1] = 0
 
             elif (int(data[x][y+1]) == 0):
-                print("the next value is 0")
-                
-                        
-                        
-print(data)
+                # print("the next value is 0")
+                if(int(data[x][len(data)-2]) != 0):
+                    data[x][y+1] = data[x][len(data)-2]
+                    data[x][len(data)-2] = 0
+                    data[x][y] = int(data[x][y]) + int(data[x][y+1])
+                    data[x][y+1] = 0
+                    # print("if one")
+                    if(int(data[x][len(data)-1]) != 0):
+                        data[x][y+1] = data[x][len(data)-1]
+                        data[x][len(data)-1] = 0
+                        # print("if two")
+                elif(int(data[x][len(data)-2]) == 0 and int(data[x][len(data)-1]) != 0):
+                    data[x][y+1] = data[x][len(data)-1]
+                    data[x][len(data)-1] = 0
+                    data[x][y] = int(data[x][y]) + int(data[x][y+1])
+                    data[x][y+1] = 0
+                    # print("else one")
 
+        if(direction == 2):
+            if(data[x][len(data)-y-1] == data[x][len(data)-y-2]):
+                data[x][len(data)-y-1] = int(data[x][len(data)-y-1]) + int(data[x][len(data)-y-2])
+                data[x][len(data)-y-1] = data[x][y+1]
+                data[x][y+1] = data[x][y]
+                data[x][y] = 0
+
+                if(int(data[x][y+1]) != 0 and int(data[x][len(data)-2]) == 0):
+                    print("data[x][y] still have number")
+                    data[x][len(data)-2] = data[x][y+1]
+                    data[x][y+1] = 0
+
+            elif (int(data[x][len(data)-2]) == 0):
+                # print("the next value is 0")
+                if(int(data[x][y+1]) != 0):
+                    data[x][len(data)-2] = data[x][y+1]
+                    data[x][y+1] = 0
+                    data[x][len(data)-1] = int(data[x][len(data)-1]
+                                               ) + int(data[x][len(data)-2])
+                    data[x][len(data)-2] = 0
+                    # print("if one")
+                    if(int(data[x][y]) != 0):
+                        data[x][len(data)-2] = data[x][y]
+                        data[x][y] = 0
+                        # print("if two")
+                elif(int(data[x][y+1]) == 0 and int(data[x][y]) != 0):
+                    data[x][len(data)-2] = data[x][y]
+                    data[x][y] = 0
+                    data[x][len(data)-1] = int(data[x][len(data)-1]
+                                               ) + int(data[x][len(data)-2])
+                    data[x][len(data)-2] = 0
+                    # print("else one")
+
+print(data)
